9e1b179fc4eead89f0133946c7b0aade51756798
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2021-11-30 23:24:48 +0600

    update description

--- src/main/java/io/github/tahanima/config/package-info.java
+++ src/main/java/io/github/tahanima/config/package-info.java
@@ -1,9 +1,9 @@
 /**
- * This module facilitates the handling of properties-based configuration
+ * This package facilitates the handling of properties-based configuration
  *
  * <p>
  * There are some global parameters that are subject to change, for example: web browser.
- * And these global parameters are key components of the project. This module intends to
+ * And these global parameters are key components of the project. This package intends to
  * provide a reader for the global parameters.
  * </p>
  *
