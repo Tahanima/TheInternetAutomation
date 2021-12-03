88f1d31eaacd6b58da5347f5fb5041e23588431b
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2021-12-03 12:27:04 +0600

    Add test for AddRemoveElementsPage

---
+++ src/test/java/io/github/tahanima/AddRemoveElementsTest.java
@@ -0,0 +1,52 @@
+package io.github.tahanima;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import io.github.tahanima.page.AddRemoveElementsPage;
+import org.testng.annotations.Test;
+
+/**
+ * @author tahanima
+ * @since 12/02/2021
+ */
+public final class AddRemoveElementsTest extends BaseTest {
+    private AddRemoveElementsPage addRemoveElementsPage;
+
+    @Override
+    public void initialize() {
+        addRemoveElementsPage = createInstance(AddRemoveElementsPage.class);
+    }
+
+    @Test(description = "Clicking 'Add Element' adds a 'Delete' button")
+    public void testAddDeleteButton() {
+        addRemoveElementsPage.goTo();
+
+        int initialDeleteButtonCount =
+                addRemoveElementsPage.countOfDeleteButtons();
+
+        addRemoveElementsPage.addDeleteButton();
+
+        int finalDeleteButtonCount =
+                addRemoveElementsPage.countOfDeleteButtons();
+
+        assertThat(finalDeleteButtonCount)
+                .isEqualTo(initialDeleteButtonCount + 1);
+    }
+
+    @Test(description = "Clicking 'Delete' button removes one 'Delete' button")
+    public void testRemoveDeleteButton() {
+        addRemoveElementsPage.goTo();
+        addRemoveElementsPage.addDeleteButton();
+
+        int initialDeleteButtonCount =
+                addRemoveElementsPage.countOfDeleteButtons();
+
+        addRemoveElementsPage.removeDeleteButton();
+
+        int finalDeleteButtonCount =
+                addRemoveElementsPage.countOfDeleteButtons();
+
+        assertThat(finalDeleteButtonCount)
+                .isEqualTo(initialDeleteButtonCount - 1);
+    }
+}
