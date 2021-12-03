c77ca3ae0f7e3325f62ee2346719bd6ba011578a
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2021-12-03 12:25:44 +0600

    add url path for add/remove elements page

---
+++ src/main/java/io/github/tahanima/util/UrlConstants.java
@@ -0,0 +1,11 @@
+package io.github.tahanima.util;
+
+/**
+ * @author tahanima
+ * @since 12/02/2021
+ */
+public final class UrlConstants {
+    private UrlConstants() { }
+
+    public static final String ADD_REMOVE_ELEMENTS_URL = "add_remove_elements/";
+}
